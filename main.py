ee85c9b0c039d63c2cf5cfaf11fd8a4cefcff1a5
Peko Chama <99276653+AleMar21430@users.noreply.github.com>
2023-10-21 09:10:17 -0600

    Commit

--- main.py
+++ main.py
@@ -1,107 +1,112 @@
 from typing import Dict, List
 
-def remove_useless_productions(cfg: Dict[str,List[str]]):
-	# Step 1: Identify reachable non-terminals
-	reachable_nonterminals = set()
-	pending = ['S']
+class Grammar :
+	nonTerminals: Dict[str,List[str]] = {}
 
-	while pending:
-		current = pending.pop()
-		for production in cfg[current]:
-			for symbol in production:
-				if symbol.isupper() and symbol not in reachable_nonterminals:
-					reachable_nonterminals.add(symbol)
-					pending.append(symbol)
+	def addRule(self, rule) :
+		nt = False
+		parse = ""
+		name = ""
+		for i in range(len(rule)) :
+			c = rule[i]
+			if c == ' ' :
+				if not nt :
+					self.nonTerminals[parse] = []
+					name = parse
+					nt = True
+					parse = ""
+				elif parse != "" :
+					self.nonTerminals[name].append(parse)
+					parse = ""
+			elif c != '|' and c != '-' and c != '>' : parse += c
+		if parse != "" : self.nonTerminals[name].append(parse)
 
-	# Step 2: Identify reachable productions
-	new_cfg = {}
-	for nonterminal, productions in cfg.items():
-		if nonterminal in reachable_nonterminals:
-			new_productions = []
-			for production in productions:
-				if all(symbol in reachable_nonterminals or not symbol.isupper() for symbol in production):
-					new_productions.append(production)
-			if new_productions:
-				new_cfg[nonterminal] = new_productions
+	def remove_useless_productions(self):
+		# Step 1: Identify reachable non-terminals
+		reachable_nonterminals = set()
+		pending = ['S']
 
-	# Step 3: Remove unused non-terminals
-	for nonterminal in cfg.keys():
-		if nonterminal not in reachable_nonterminals:
-			new_cfg.pop(nonterminal, None)
+		while pending:
+			current = pending.pop()
+			for production in self.nonTerminals[current]:
+				for symbol in production:
+					if symbol.isupper() and symbol not in reachable_nonterminals:
+						reachable_nonterminals.add(symbol)
+						pending.append(symbol)
 
-	first_item = next(iter(cfg.items()))
-	new_cfg = {first_item[0]: first_item[1], **new_cfg}
-	return new_cfg
+		# Step 2: Identify reachable productions
+		new_cfg = {}
+		for nonterminal, productions in self.nonTerminals.items():
+			if nonterminal in reachable_nonterminals:
+				new_productions = []
+				for production in productions:
+					if all(symbol in reachable_nonterminals or not symbol.isupper() for symbol in production):
+						new_productions.append(production)
+				if new_productions:
+					new_cfg[nonterminal] = new_productions
 
+		# Step 3: Remove unused non-terminals
+		for nonterminal in self.nonTerminals.keys():
+			if nonterminal not in reachable_nonterminals:
+				new_cfg.pop(nonterminal, None)
 
-def remove_epsilon_productions(cfg: Dict[str, List[str]]):
-	# Step 1: Find nullable variables
-	nullable = set()
-	for variable, productions in cfg.items():
-		if '' or 'ε' in productions:
-			nullable.add(variable)
+		first_item = next(iter(self.nonTerminals.items()))
+		new_cfg = {first_item[0]: first_item[1], **new_cfg}
+		self.nonTerminals = new_cfg
 
-	# Step 2: Remove productions containing nullable variables
-	new_cfg = cfg.copy()
-	for variable, productions in cfg.items():
-		updated_productions = [p for p in productions if all(s not in nullable for s in p)]
-		new_cfg[variable] = updated_productions
+	def remove_epsilon_productions(self):
+		# Step 1: Find nullable variables
+		nullable = set()
+		for variable, productions in self.nonTerminals.items():
+			if '' or 'ε' in productions:
+				nullable.add(variable)
 
-	# Step 3: Update other productions
-	for variable, productions in cfg.items():
-		for i in range(len(productions)):
-			for j in range(1, len(productions[i]) + 1):
-				# Try to find nullable variables
-				for k in range(j):
-					prefix = productions[i][:k]
-					suffix = productions[i][k + 1:]
-					if all(s not in nullable for s in prefix) and all(s not in nullable for s in suffix):
-						updated_production = prefix + suffix
-						if updated_production not in new_cfg[variable]:
-							new_cfg[variable].append(updated_production)
+		# Step 2: Remove productions containing nullable variables
+		new_cfg = self.nonTerminals.copy()
+		for variable, productions in self.nonTerminals.items():
+			updated_productions = [p for p in productions if all(s not in nullable for s in p)]
+			new_cfg[variable] = updated_productions
 
-	# Remove empty productions
-	for variable, productions in new_cfg.items():
-		new_cfg[variable] = [p for p in productions if p != '']
+		# Step 3: Update other productions
+		for variable, productions in self.nonTerminals.items():
+			for i in range(len(productions)):
+				for j in range(1, len(productions[i]) + 1):
+					# Try to find nullable variables
+					for k in range(j):
+						prefix = productions[i][:k]
+						suffix = productions[i][k + 1:]
+						if all(s not in nullable for s in prefix) and all(s not in nullable for s in suffix):
+							updated_production = prefix + suffix
+							if updated_production not in new_cfg[variable]:
+								new_cfg[variable].append(updated_production)
 
-	return new_cfg
+		# Remove empty productions
+		for variable, productions in new_cfg.items():
+			new_cfg[variable] = [p for p in productions if p != '']
+
+		self.nonTerminals = new_cfg
+
+	def toCNF(self):
+		self.remove_useless_productions()
+		#self.remove_epsilon_productions()
+		return self.nonTerminals
 
 # MAIN
-def cfg_to_cnf(cfg):
-	cfg = remove_useless_productions(cfg)  # CORRECT
-	cfg = remove_epsilon_productions(cfg)  # TODO: does not eliminate ε,  removes AB BC
+def cfg_to_cnf(cfg: Grammar):
+	cfg = cfg.remove_useless_productions()  # CORRECT
+	cfg = cfg.remove_epsilon_productions()  # TODO: does not eliminate ε, removes AB BC
 	return cfg
 
-cfg = {
-	'S'  : ['NP VP'],
-	'VP' : ['VP PP', 'V NP', 'cooks', 'drinks', 'eats', 'cuts'],
-	'PP' : ['P NP'],
-	'NP' : ['Det N', 'it', 'she'],
-	'V'  : ['cooks', 'drinks', 'eats', 'cuts'],
-	'P'  : ['in', 'with'],
-	'N'  : ['cat', 'dog', 'beer', 'cake', 'juice', 'meat', 'soup', 'fork', 'knife', 'oven', 'spoon'],
-	'Det': ['a', 'the']
-}
-
-cfg = {
-	'S': ['AB', 'BC', 'AB BC'],
-	'A': ['aA', 'a'],
-	'B': ['bB', 'bC', 'c', 'ε'],
-	'C': ['cC', 'c'],
-	'D': ['d']
-	
-}
+cfg = Grammar()
+for line in open("test.txt", "r", -1, "utf-8").readlines():
+	cfg.addRule(line)
 
 #src: https://www.geeksforgeeks.org/simplifying-context-free-grammars/
 #src: https://www.geeksforgeeks.org/converting-context-free-grammar-chomsky-normal-form/
 
-cnf_grammar = cfg_to_cnf(cfg)
+cnf_grammar = cfg.toCNF()
 lines = []
 for non_terminal, productions in cnf_grammar.items():
-	for production in productions:
-		lines.append(f"{non_terminal} -> {production}\n")
-
-lines[-1] = lines[-1][:-1]
-with open('cnf.txt', 'w', encoding='utf-8') as file:
-	for line in lines:
-		file.write(line)
+	line = f"{non_terminal} -> { ' | '.join(productions)}"
+	print(line, end="")
+	lines.append(f"{line}\n")
